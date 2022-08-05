9ba17a45e122bfd78adfd1a8fc4f682a4743d1c0
iamnicky <2020ugee067@nitjsr.ac.in>
2022-08-05 23:14:06 +0530

    contact updated

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -4,18 +4,21 @@ function Details() {
   return (
     <div>
        
-<div className='h-[540px] m-8 bg-white p-4 flex shadow-xl shadow-blue-500'>
+<div className='h-auto m-8 bg-white p-4 flex shadow-xl shadow-blue-500'>
 <h1 className='text-black font-bold text-5xl flex items-center text-wrap  mx-4 mx-8 p-16 m-2 m-2 '>Lets talk about everything!</h1>
 
 
 <div className=' p-16 m-8'>
+<h1 className='capitalize text-3xl font-bold font-mono text-black'>waiting for you to connect!</h1>
 <div className='mt-8 space-x-4'>
+
 <label for="email" className="mb-2 text-lg font-medium text-gray-900 text-xl ">Your email</label>
 <input type="email" name="email" id="email" className='bg-black w-80 text-xl p-2 font-mono ' placeholder="username@gamil.com" required></input>
 
 
 </div>
 <div className='space-x-4 mt-8'>
+ 
 <label for="password" className="mb-2 text-lg font-medium text-gray-900 text-xl ">Your password</label>
 <input type="password" name="password" id="password" placeholder="••••••••" className='bg-black w-80 text-xl p-2'/>
 </div>
