ba41216752d5f854ec26e48e839a107bd3502a43
iamnicky <2020ugee067@nitjsr.ac.in>
2022-08-08 22:22:21 +0530

    updated

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -5,26 +5,23 @@ function Try() {
     <div className=' w-full mt-8'>
       <div class="flex space-x-4 m-2 ">
   <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm hover:shadow-lg hover:shadow-cyan-500/50 ">
-    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Card title</h5>
+    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">MyBlog</h5>
     <p class="text-gray-700 text-base mb-4">
-      Some quick example text to build on the card title and make up the bulk of the card's
-      content.
+     This is my blog wesite where you will get all the blogs about  Full Stack Development as well as Android Development.
     </p>
     <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
   </div>
   <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm hover:shadow-lg hover:shadow-cyan-500/50 ">
-    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Card title</h5>
+    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Weather App</h5>
     <p class="text-gray-700 text-base mb-4">
-      Some quick example text to build on the card title and make up the bulk of the card's
-      content.
+      A simple weather app created with the help of ReactJS , Axios and Tailwind CSS.
     </p>
     <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
   </div>
   <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm hover:shadow-lg hover:shadow-cyan-500/50 ">
-    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Card title</h5>
+    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">My Crypto App </h5>
     <p class="text-gray-700 text-base mb-4">
-      Some quick example text to build on the card title and make up the bulk of the card's
-      content.
+    A reactJS project using axios and Crypto API's.where you can search yr interest Crypto . 
     </p>
     <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
   </div>
