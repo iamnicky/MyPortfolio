74c3674bd644eabff65f208418fd9117ecdf028d
iamnicky <2020ugee067@nitjsr.ac.in>
2022-08-10 22:13:24 +0530

    update

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -9,21 +9,21 @@ function Try() {
     <p class="text-gray-700 text-base mb-4">
      This is my blog wesite where you will get all the blogs about  Full Stack Development as well as Android Development.
     </p>
-    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Github</button>
   </div>
   <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm hover:shadow-lg hover:shadow-cyan-500/50 ">
     <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Weather App</h5>
     <p class="text-gray-700 text-base mb-4">
       A simple weather app created with the help of ReactJS , Axios and Tailwind CSS.
     </p>
-    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Github</button>
   </div>
   <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm hover:shadow-lg hover:shadow-cyan-500/50 ">
     <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">My Crypto App </h5>
     <p class="text-gray-700 text-base mb-4">
     A reactJS project using axios and Crypto API's.where you can search yr interest Crypto . 
     </p>
-    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Github</button>
   </div>
 </div>
         
